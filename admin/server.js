2963b83b24e87968ffca782ece9720408e587077
abhijeetsachan <abhijeetsachan17may@gmail.com>
2025-11-06 01:41:26 +0530

    Update server.js

--- admin/server.js
+++ admin/server.js
@@ -149,6 +149,21 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ extended: true }));
 
+// --- NEW: SET CONTENT SECURITY POLICY (CSP) ---
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; " +
+    "script-src 'self' https://cdn.tailwindcss.com https://unpkg.com https://cdn.tiny.cloud 'unsafe-eval'; " +
+    "style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com 'unsafe-inline'; " +
+    "font-src 'self' https://fonts.gstatic.com; " +
+    "img-src 'self' https: data:; " +
+    "connect-src 'self' https://raw.githubusercontent.com; " +
+    "frame-src 'self' https://cdn.tiny.cloud"
+  );
+  next();
+});
+
 // --- Session Middleware ---
 app.use(session({
     secret: SESSION_SECRET,
@@ -839,3 +854,4 @@ app.listen(PORT, () => {
     console.log(\`Admin Login: http://localhost:\${PORT}/admin/login.html\`);
     console.log(\`-------------------------------------------------\`);
 });
+
